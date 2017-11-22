3bdd25171fb1c2ab112b7c8fb4fdc34102faead1
CHUBBY799 <chubby799@163.com>
2017-11-22 14:24:06 +0800

    装箱拆箱理解

--- src/javaLearn.java
+++ src/javaLearn.java
@@ -1,9 +1,18 @@
+import java.lang.reflect.Field;
 import java.util.*;
 
 //请完成上传一份Java的代码，
 // 代码包括
 // Java Random，Math，String类相关成员函数的使用，
 // ArrayList，Set，HashMap等各种容器类的使用。
+class  A{
+    protected  int i;
+
+    A(int i){
+        this.i=i;
+
+    }
+}
 public class javaLearn {
     public static void print(int i,Object object){
         System.out.println(String.format("{%d}, %s",i,object));
@@ -53,20 +62,28 @@ public class javaLearn {
         print(12,queue.element());
     }
     public static  void listLearn(){
-        List<Integer> list=new ArrayList<Integer>();
-        for(int i=0;i<10;i++){
-            list.add(i);
+        List<Integer> list=new LinkedList<Integer>();
+        list.add(1);
+        list.add(4);
+        list.add(2);
+        list.add(3);
+        for(int i=0;i<4;i++) {
+            print(1, list.get(i));
         }
+
+//        for(int i=0;i<10;i++){
+//            list.add(i);
+//        }
         Iterator iterator=list.iterator();
         int i=0;
         while ((iterator.hasNext())){
             print(i,iterator.next());
             i++;
         }
-        while(i<20){
-            print(i,list.toArray()[i-10]);
-            i++;
-        }
+//        while(i<20){
+//            print(i,list.toArray()[i-10]);
+//            i++;
+//        }
 //        int b=10;
 //        while ((iterator.hasNext())){
 //            print(b,iterator.next());
@@ -88,15 +105,72 @@ public class javaLearn {
         }
 
     }
-    public static void main(String[] args) {
-
-//        print(1,"woaini");
-//        randomLearn();
-//       mathLearn();
-//         stringLearn();
-//
-//     queueLearn();
-//        listLearn();
-        mapLearn();
+    public static int _add(int i,int j){
+        return  i+j;
     }
+    public static void listIteratorLearn(){
+        List<String> list=new LinkedList<>();
+        list.add("amy");
+        list.add("jack");
+        list.add("wo");
+        list.add("ai");
+        int i=0;
+        for(String s:list){
+            print(i,s);
+            i++;
+        }
+        ListIterator listIterator=list.listIterator();
+
+
+    while(listIterator.hasNext()){
+        print(1,listIterator.next());
+//        print(2,listIterator.previous());
+    }
+
+    }
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+//
+//       A a =new A(15);
+//       A b=new A(16);
+//        System.out.println(a+" "+a.i);
+//        System.out.println(b+" "+b.i);
+//
+//        swap(a,b);
+//        System.out.println(a+" "+a.i);
+//        System.out.println(b+" "+b.i);
+//        对包装类装箱拆箱的理解 不可删
+        Integer A=1;
+        Integer B=2;
+        Field field=Integer.class.getDeclaredField("value");
+        field.setAccessible(true);
+        field.set(A,B);
+        Integer C=1;
+        System.out.println(C.intValue());
+
+
+
+
+
+    }
+    public  static void swap(A numA,A numB){
+//        System.out.println(numA);
+//        System.out.println(numB);
+        A temp=numA;
+        numA=numB;
+        numB=temp;
+       System.out.println(numA);
+       System.out.println(numB);
+
+    }
+    public  static void swap(int numA,int numB){
+//        System.out.println(numA);
+//        System.out.println(numB);
+        int temp=numA;
+        numA=numB;
+        numB=temp;
+//        System.out.println(numA);
+//        System.out.println(numB);
+
+    }
+
 }
